c8c0374155ff1b56fcecdf9eb9812f6fb203c461
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-10 18:42:24 +0200

    fixed grid

--- js/script.js
+++ js/script.js
@@ -17,31 +17,87 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 */
 
 const button = document.getElementById('create-grid-btn');
+const grid = document.querySelector('#grid')
 
-button.addEventListener('click', function () {
-    console.log('clicked');
+button.addEventListener('click', myFunction);
 
-    const gridElement = createGrid();
+function myFunction() {
+    console.log(this);
 
-    //creare celle da 1 a 100
-    for (let i = 0; i <= 100; i++) {
-        const cellElement = createCell(i);
-
-        gridElement.append(cellElement);
+    for (let i = 1; i <= 100; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        grid.innerHTML += '<div class="cell">' + i + '</div>'
+        // return cell;
     }
-}
-)
 
-const createGrid = () => {
-    const element = document.getElementById('grid');
-    element.innerHTML = '';
-    return element;
+    let cellsAll = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < cellsAll.length; i++) {
+        let cellNumber = parseInt(cellsAll[i]).innerHTML;
+
+        console.log(cellNumber);
+
+        if (cellNumber % 2 === 0) {
+            cell.classList.add('even');
+        } else {
+            cell.classList.add('odd');
+        }
+
+    }
+
+
+
+
+
 }
 
-function isEven(number) {
-    return number % 2 === 0;
-}
+// function myFunction2() {
+// }
 
-function isOdd(number) {
-    return number % 2 !== 0;
-}
+
+//     const gridElement = createGrid();
+
+//     const cellValues = getRandomNumbersArray();
+
+//     //creare celle da 1 a 100
+//     for (let index = 0; index < cellValues.length; index++) {
+//         const numberToDisplay = cellValues[index];
+//         const cellElement = createCell(numberToDisplay);
+
+//         //aggiungo l'elemento alla grid
+//         gridElement.append(cellElement);
+//     }
+// }
+// )
+
+// function getRandomNumbersArrayAlternative() {
+//     const array = [];
+//     for (let index = 1; index <= 100; index++) {
+//         array.push[index];
+
+//     }
+
+//     array.sort(function () {
+//         const random = Math.round(Math.random());
+//         return random === 1 ? 1 : -1
+//     })
+//     console.log(array);
+
+//     return (array);
+
+// }
+
+// const createGrid = () => {
+//     const element = document.getElementById('grid');
+//     element.innerHTML = '';
+//     return element;
+// }
+
+// function isEven(number) {
+//     return number % 2 === 0;
+// }
+
+// function isOdd(number) {
+//     return number % 2 !== 0;
+// }
